e2ea8c1207820476f00cf9c4a2d7e208c10cc5d4
xuliaP <paukov2967@gmail.com>
2016-01-06 13:31:45 +0300

    0601

--- config/initializers/rails_admin.rb
+++ config/initializers/rails_admin.rb
@@ -3,4 +3,14 @@ RailsAdmin.config do |config|
     warden.authenticate! scope: :admin
   end
   config.current_user_method(&:current_admin)
+  config.model Team do
+    edit do
+      # For RailsAdmin >= 0.5.0
+      field :description, :ck_editor
+      # For RailsAdmin < 0.5.0
+      # field :description do
+      #   ckeditor true
+      # end
+    end
+  end
 end
